3cf0578c5346fb9441b08920af3397888328e7dc
Tomás Ponce <poncetomaseduardo@gmail.com>
2020-05-21 22:14:52 -0300

    Se colocaron los nombres de las tablas en plural

--- biblioteca_BD.sql
+++ biblioteca_BD.sql
@@ -18,38 +18,38 @@ USE `biblioteca`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `ejemplar`
+-- Table structure for table `ejemplares`
 --
 
-DROP TABLE IF EXISTS `ejemplar`;
+DROP TABLE IF EXISTS `ejemplares`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `ejemplar` (
+CREATE TABLE `ejemplares` (
   `id_ejemplar` int(11) NOT NULL,
   `id_libro` int(11) NOT NULL,
   PRIMARY KEY (`id_ejemplar`,`id_libro`),
   KEY `idLibro_idx` (`id_libro`),
-  CONSTRAINT `fk_libro_ejemplar` FOREIGN KEY (`id_libro`) REFERENCES `libro` (`id_libro`) ON UPDATE CASCADE
+  CONSTRAINT `fk_libro_ejemplar` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id_libro`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `ejemplar`
+-- Dumping data for table `ejemplares`
 --
 
-LOCK TABLES `ejemplar` WRITE;
-/*!40000 ALTER TABLE `ejemplar` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ejemplar` ENABLE KEYS */;
+LOCK TABLES `ejemplares` WRITE;
+/*!40000 ALTER TABLE `ejemplares` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ejemplares` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `genero`
+-- Table structure for table `generos`
 --
 
-DROP TABLE IF EXISTS `genero`;
+DROP TABLE IF EXISTS `generos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `genero` (
+CREATE TABLE `generos` (
   `id_genero` int(11) NOT NULL,
   `descripcion` varchar(125) NOT NULL,
   PRIMARY KEY (`id_genero`)
@@ -57,42 +57,12 @@ CREATE TABLE `genero` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `genero`
+-- Dumping data for table `generos`
 --
 
-LOCK TABLES `genero` WRITE;
-/*!40000 ALTER TABLE `genero` DISABLE KEYS */;
-/*!40000 ALTER TABLE `genero` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `libro`
---
-
-DROP TABLE IF EXISTS `libro`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `libro` (
-  `id_libro` int(11) NOT NULL,
-  `autor` varchar(125) NOT NULL,
-  `titulo` varchar(125) NOT NULL,
-  `nro_edicion` int(11) NOT NULL,
-  `fecha_edicion` date NOT NULL,
-  `cant_dias_max` int(11) NOT NULL,
-  `id_genero` int(11) NOT NULL,
-  PRIMARY KEY (`id_libro`),
-  KEY `idGenero_idx` (`id_genero`),
-  CONSTRAINT `fk_genero_libro` FOREIGN KEY (`id_genero`) REFERENCES `genero` (`id_genero`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `libro`
---
-
-LOCK TABLES `libro` WRITE;
-/*!40000 ALTER TABLE `libro` DISABLE KEYS */;
-/*!40000 ALTER TABLE `libro` ENABLE KEYS */;
+LOCK TABLES `generos` WRITE;
+/*!40000 ALTER TABLE `generos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `generos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -109,8 +79,8 @@ CREATE TABLE `libro_reserva` (
   PRIMARY KEY (`id_libro_reserva`),
   KEY `fk_reserva_libroreserva_idx` (`id_reserva`),
   KEY `fk_libro_libroreserva_idx` (`id_libro`),
-  CONSTRAINT `fk_libro_libroreserva` FOREIGN KEY (`id_libro`) REFERENCES `libro` (`id_libro`) ON UPDATE CASCADE,
-  CONSTRAINT `fk_reserva_libroreserva` FOREIGN KEY (`id_reserva`) REFERENCES `reserva` (`id_reserva`) ON UPDATE CASCADE
+  CONSTRAINT `fk_libro_libroreserva` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id_libro`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_reserva_libroreserva` FOREIGN KEY (`id_reserva`) REFERENCES `reservas` (`id_reserva`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -124,13 +94,43 @@ LOCK TABLES `libro_reserva` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `lineadeprestamo`
+-- Table structure for table `libros`
 --
 
-DROP TABLE IF EXISTS `lineadeprestamo`;
+DROP TABLE IF EXISTS `libros`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `lineadeprestamo` (
+CREATE TABLE `libros` (
+  `id_libro` int(11) NOT NULL,
+  `autor` varchar(125) NOT NULL,
+  `titulo` varchar(125) NOT NULL,
+  `nro_edicion` int(11) NOT NULL,
+  `fecha_edicion` date NOT NULL,
+  `cant_dias_max` int(11) NOT NULL,
+  `id_genero` int(11) NOT NULL,
+  PRIMARY KEY (`id_libro`),
+  KEY `idGenero_idx` (`id_genero`),
+  CONSTRAINT `fk_genero_libro` FOREIGN KEY (`id_genero`) REFERENCES `generos` (`id_genero`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libros`
+--
+
+LOCK TABLES `libros` WRITE;
+/*!40000 ALTER TABLE `libros` DISABLE KEYS */;
+/*!40000 ALTER TABLE `libros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lineasdeprestamo`
+--
+
+DROP TABLE IF EXISTS `lineasdeprestamo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `lineasdeprestamo` (
   `id_lineadeprestamo` int(11) NOT NULL,
   `id_ejemplar` int(11) NOT NULL,
   `id_libro` int(11) NOT NULL,
@@ -140,18 +140,18 @@ CREATE TABLE `lineadeprestamo` (
   PRIMARY KEY (`id_lineadeprestamo`,`id_prestamo`,`id_libro`,`id_ejemplar`),
   KEY `fk_lineadeprestamo_ejemplar_idx` (`id_ejemplar`,`id_libro`),
   KEY `fk_lineadeprestamo_prestamo_idx` (`id_prestamo`),
-  CONSTRAINT `fk_ejemplar_lineadeprestamo` FOREIGN KEY (`id_ejemplar`) REFERENCES `ejemplar` (`id_ejemplar`) ON UPDATE CASCADE,
-  CONSTRAINT `fk_prestamo_lineadeprestamo` FOREIGN KEY (`id_prestamo`) REFERENCES `prestamo` (`id_prestamo`) ON UPDATE CASCADE
+  CONSTRAINT `fk_ejemplar_lineadeprestamo` FOREIGN KEY (`id_ejemplar`) REFERENCES `ejemplares` (`id_ejemplar`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_prestamo_lineadeprestamo` FOREIGN KEY (`id_prestamo`) REFERENCES `prestamos` (`id_prestamo`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `lineadeprestamo`
+-- Dumping data for table `lineasdeprestamo`
 --
 
-LOCK TABLES `lineadeprestamo` WRITE;
-/*!40000 ALTER TABLE `lineadeprestamo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `lineadeprestamo` ENABLE KEYS */;
+LOCK TABLES `lineasdeprestamo` WRITE;
+/*!40000 ALTER TABLE `lineasdeprestamo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lineasdeprestamo` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -203,13 +203,13 @@ LOCK TABLES `politicasancion` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `prestamo`
+-- Table structure for table `prestamos`
 --
 
-DROP TABLE IF EXISTS `prestamo`;
+DROP TABLE IF EXISTS `prestamos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `prestamo` (
+CREATE TABLE `prestamos` (
   `id_prestamo` int(11) NOT NULL,
   `fecha_prestamo` date NOT NULL,
   `dias_prestamo` int(11) NOT NULL,
@@ -217,81 +217,81 @@ CREATE TABLE `prestamo` (
   `id_socio` int(11) NOT NULL,
   PRIMARY KEY (`id_prestamo`,`id_socio`),
   KEY `fk_prestamo_socio_idx` (`id_socio`),
-  CONSTRAINT `fk_socio_prestamo` FOREIGN KEY (`id_socio`) REFERENCES `socio` (`id_socio`) ON UPDATE CASCADE
+  CONSTRAINT `fk_socio_prestamo` FOREIGN KEY (`id_socio`) REFERENCES `socios` (`id_socio`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `prestamo`
+-- Dumping data for table `prestamos`
 --
 
-LOCK TABLES `prestamo` WRITE;
-/*!40000 ALTER TABLE `prestamo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `prestamo` ENABLE KEYS */;
+LOCK TABLES `prestamos` WRITE;
+/*!40000 ALTER TABLE `prestamos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prestamos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `reserva`
+-- Table structure for table `reservas`
 --
 
-DROP TABLE IF EXISTS `reserva`;
+DROP TABLE IF EXISTS `reservas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `reserva` (
+CREATE TABLE `reservas` (
   `id_reserva` int(11) NOT NULL,
   `fecha_reserva` date NOT NULL,
   `entregada` bit(1) NOT NULL,
   `id_socio` int(11) NOT NULL,
   PRIMARY KEY (`id_reserva`),
   KEY `fk_socio_reserva_idx` (`id_socio`),
-  CONSTRAINT `fk_socio_reserva` FOREIGN KEY (`id_socio`) REFERENCES `socio` (`id_socio`) ON UPDATE CASCADE
+  CONSTRAINT `fk_socio_reserva` FOREIGN KEY (`id_socio`) REFERENCES `socios` (`id_socio`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `reserva`
+-- Dumping data for table `reservas`
 --
 
-LOCK TABLES `reserva` WRITE;
-/*!40000 ALTER TABLE `reserva` DISABLE KEYS */;
-/*!40000 ALTER TABLE `reserva` ENABLE KEYS */;
+LOCK TABLES `reservas` WRITE;
+/*!40000 ALTER TABLE `reservas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservas` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `sancion`
+-- Table structure for table `sanciones`
 --
 
-DROP TABLE IF EXISTS `sancion`;
+DROP TABLE IF EXISTS `sanciones`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `sancion` (
+CREATE TABLE `sanciones` (
   `id_sancion` int(11) NOT NULL,
   `id_socio` int(11) NOT NULL,
   `fecha_sancion` date NOT NULL,
   `dias_sancion` int(11) NOT NULL,
   PRIMARY KEY (`id_sancion`,`id_socio`),
   KEY `fk_sancion_socio_idx` (`id_socio`),
-  CONSTRAINT `fk_socio_sancion` FOREIGN KEY (`id_socio`) REFERENCES `socio` (`id_socio`) ON UPDATE CASCADE
+  CONSTRAINT `fk_socio_sancion` FOREIGN KEY (`id_socio`) REFERENCES `socios` (`id_socio`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `sancion`
+-- Dumping data for table `sanciones`
 --
 
-LOCK TABLES `sancion` WRITE;
-/*!40000 ALTER TABLE `sancion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sancion` ENABLE KEYS */;
+LOCK TABLES `sanciones` WRITE;
+/*!40000 ALTER TABLE `sanciones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sanciones` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `socio`
+-- Table structure for table `socios`
 --
 
-DROP TABLE IF EXISTS `socio`;
+DROP TABLE IF EXISTS `socios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `socio` (
+CREATE TABLE `socios` (
   `id_socio` int(11) NOT NULL,
   `nombre` varchar(125) NOT NULL,
   `apellido` varchar(125) NOT NULL,
@@ -303,27 +303,27 @@ CREATE TABLE `socio` (
   `id_usuario` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_socio`),
   KEY `fk_usuario_socio_idx` (`id_usuario`),
-  CONSTRAINT `fk_usuario_socio` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON UPDATE CASCADE
+  CONSTRAINT `fk_usuario_socio` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `socio`
+-- Dumping data for table `socios`
 --
 
-LOCK TABLES `socio` WRITE;
-/*!40000 ALTER TABLE `socio` DISABLE KEYS */;
-/*!40000 ALTER TABLE `socio` ENABLE KEYS */;
+LOCK TABLES `socios` WRITE;
+/*!40000 ALTER TABLE `socios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `socios` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `usuario`
+-- Table structure for table `usuarios`
 --
 
-DROP TABLE IF EXISTS `usuario`;
+DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `usuario` (
+CREATE TABLE `usuarios` (
   `id_usuario` int(11) NOT NULL,
   `nombre_usuario` varchar(125) NOT NULL,
   `password` varchar(125) NOT NULL,
@@ -334,12 +334,12 @@ CREATE TABLE `usuario` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `usuario`
+-- Dumping data for table `usuarios`
 --
 
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -351,4 +351,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-21 18:15:02
+-- Dump completed on 2020-05-21 22:12:37
