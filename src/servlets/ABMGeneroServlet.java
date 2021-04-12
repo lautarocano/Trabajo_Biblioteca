3650d8d994b794b9f22333e3d673aa4be009ad12
Tomas Ponce <poncetomaseduardo@gmail.com>
2021-04-12 20:01:38 -0300

    Validación de ABM Genero

--- src/servlets/ABMGeneroServlet.java
+++ src/servlets/ABMGeneroServlet.java
@@ -49,47 +49,90 @@ public class ABMGeneroServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		if (Servlet.VerificarSesionYUsuario(request, response, Usuario.tipoUsuario.Administrador)) {
 			if (request.getParameter("action-type").equals("agregar")) {	
+				if (ValidarDatos(request)) {
+
 				Genero genero=new Genero();
 				genero.setDescripcion(request.getParameter("descripcion"));
 				GeneroLogic gl=new GeneroLogic();
-				try {
-					gl.insert(genero);
-					request.setAttribute("clase-mensaje", "class=\"alert alert-success alert-dismissible fade show\"");
-					request.setAttribute("mensaje", "Genero agregado correctamente");
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					request.setAttribute("mensaje", "No se pudo agregar un genero");
+					try {
+						gl.insert(genero);
+						request.setAttribute("clase-mensaje", "class=\"alert alert-success alert-dismissible fade show\"");
+						request.setAttribute("mensaje", "Genero agregado correctamente");
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						request.setAttribute("mensaje", "No se pudo agregar un genero");
+					}
+					catch (Exception e) {
+						request.setAttribute("mensaje", e.getMessage());
+					}
 				}
 			}
 			else if (request.getParameter("action-type").equals("eliminar")) {	
-				Genero genero=new Genero();
-				genero.setId(Integer.parseInt(request.getParameter("id")));
+				Genero genero=null;
 				GeneroLogic gl=new GeneroLogic();
 				try {
+					genero = gl.getOne(Integer.parseInt(request.getParameter("id")));
+					if (genero != null) {
 					gl.delete(genero);
 					request.setAttribute("clase-mensaje", "class=\"alert alert-success alert-dismissible fade show\"");
 					request.setAttribute("mensaje", "Genero eliminado correctamente");
+					}
+					else {
+						request.setAttribute("clase-mensaje", "class=\"alert alert-danger alert-dismissible fade show\"");
+						request.setAttribute("mensaje", "Id de genero invalida");
+					}
+				} catch (NumberFormatException e) {
+					request.setAttribute("clase-mensaje", "class=\"alert alert-danger alert-dismissible fade show\"");
+					request.setAttribute("mensaje", "Id de genero invalida");
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
 					request.setAttribute("mensaje", "No se pudo eliminar un genero");
 				}
 			}
 			else if (request.getParameter("action-type").equals("editar")) {	
-				Genero genero=new Genero();
-				genero.setId(Integer.parseInt(request.getParameter("id")));
-				genero.setDescripcion(request.getParameter("descripcion"));
-				GeneroLogic gl=new GeneroLogic();
-				try {
-					gl.update(genero);
-					request.setAttribute("clase-mensaje", "class=\"alert alert-success alert-dismissible fade show\"");
-					request.setAttribute("mensaje", "Genero actualizado correctamente");
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					request.setAttribute("mensaje", "No se pudo actualizar el genero");
+				if (ValidarDatos(request)) {
+					GeneroLogic gl = new GeneroLogic();
+					Genero genero;
+					try {
+						genero = gl.getOne(Integer.parseInt(request.getParameter("id")));
+						if (genero != null) {
+							genero.setId(Integer.parseInt(request.getParameter("id")));
+							genero.setDescripcion(request.getParameter("descripcion"));
+							gl.update(genero);
+							request.setAttribute("clase-mensaje", "class=\"alert alert-success alert-dismissible fade show\"");
+							request.setAttribute("mensaje", "Genero actualizado correctamente");
+						}
+						else {
+							request.setAttribute("clase-mensaje", "class=\"alert alert-danger alert-dismissible fade show\"");
+							request.setAttribute("mensaje", "Id de genero invalida");
+						}
+			}
+			catch (NumberFormatException e) {
+						request.setAttribute("clase-mensaje", "class=\"alert alert-danger alert-dismissible fade show\"");
+						request.setAttribute("mensaje", "Id de genero invalida");
+			}
+			 catch (SQLException e) {
+				// TODO Auto-generated catch block
+				request.setAttribute("mensaje", "No se pudo actualizar el genero");
+			 		}	
 				}
 			}
 			this.doGet(request, response);
 		}
+		
 	}
+	
+	private static Boolean ValidarDatos (HttpServletRequest request) {
+		if (request.getParameter("descripcion")!=null) {
+			
+			request.setAttribute("mensaje", "Campos incompletos.");
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	
+
 	
 }
